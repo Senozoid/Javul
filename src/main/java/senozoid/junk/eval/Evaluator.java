e87432209f9a7a5221a2861257b54eb86c6784d6
Senozoid <93595764+Senozoid@users.noreply.github.com>
2025-05-20 23:56:00 +0530

    Several improvements to Evaluator.java:     Comments improved     Node value is now double precision     FP with signed expo supported if in parentheses     Div and mod by zero rejected early     Infinities and NaN rejected     Logarithm supports a wider range of operands     Safeguards and other minor changes

--- src/main/java/senozoid/junk/eval/Evaluator.java
+++ src/main/java/senozoid/junk/eval/Evaluator.java
@@ -4,35 +4,43 @@ import java.util.*;
 
 /*
 TODO: write docs explaining quirks and possibly unexpected behaviour, some of which are:
-    1. There is no support for assignment operators, or for floating point representations with "E".
-        Example effect 1: if "id == 2" is true, "--id" evaluates to 2.
-    2. All unary operators are prefix operators, and have higher precedence than all binary operators.
-        Example effect 1: "0-2^14" and "-2^14" evaluate to -16 and +16 respectively.
-        Example effect 2: "!(1>2)" evaluates to true, but "!1>2" is an illegal expression.
-    3. All binary operators of equal precedence are left associative.
-        Example effect 1: "24/3/4" evaluates to 2, and not 32.
-    4. Identifiers can be interpreted as either numeric or boolean operands (like int in C), but
-        each operation takes and produces values of a particular type, and the two different types of value
-        are not interconvertible or comparable (unlike int in C). This is a personal choice, to prevent hidden
-        mistakes, and strangeness with constants, like true+true being 2, which is neither true nor false.
-        Example effect 1: "id == 1" and "id == true" are both valid expressions, but "1 == true" is not.
-        Example effect 2: "!id" is a valid boolean and "-id" a valid numeric expression, but "!id != -id" is illegal.
-    5. During evaluation, identities and intermediate numeric values are stored as float and the result is
-        rounded (half up) to int. Boolean representations of identifiers are derived also by first rounding to int.
-        This is quite easy to change, and exists because the original implementation used only int variables.
-        Example effect 1: "7/2" evaluates to 4, and "7/2 == 3.5" evaluates to true.
-        Example effect 2: If "id == -0.5" evaluates to true, then "id == false" also evaluates to true.
+    1. There is no support for assignment operators, as handling identifiers is left up to you. To implement
+        handling of identifiers, see Node class constructor with a single String parameter.
+        Example 1 -> By default, "2++2" is a valid expression, and equivalent to "2+(+2)".
+    2. Floating point literals with signed exponent should be enclosed in parentheses.
+        Example 1 -> "5*1.0E-10F+10" should be written as "5*(1.0E-10F)+10".
+    3. All unary operators are prefix operators, and have higher precedence than all binary operators.
+        Example 1 -> "0-2^14" and "-2^14" evaluate to -16 and +16 respectively.
+        Example 2 -> "!(1>2)" evaluates to true, but "!1>2" is an illegal expression.
+    4. All binary operators of equal precedence are left associative.
+        Example 1 -> "x^y^z" is equivalent to "(x^y)^z", and not "x^(y^z)".
+        Example 2 -> "24 / 3 / 4" evaluates to 2, and not 32.
+    5. The possibility exists of allowing identifiers to be interpreted as either numeric or boolean
+        operands (like int in C), but each operation takes and produces values of a particular type, and the
+        two different types of value are not interconvertible or comparable (unlike int in C). To change this,
+        see usages of field Node#type.
+        Example 1 -> "1==1==true" is a valid expression, but "true==1==1" is not.
+    6. During numeric evaluation, intermediate values are stored as double precision type and the final result
+        is rounded (half up) to int, throwing an exception in case of overflow or underflow. To change this,
+        see usages of field Node#value and method Evaluator#floatToInt. If you allow numeric values to be
+        interpreted as booleans, the boolean values are derived also by first rounding to int.
+        Example 1 -> "7/2==3.5" evaluates to true, but numeric evaluation of "7/2" results in 4.
+        Example 2 -> If numeric values are made comparable to booleans, then "-0.5==false" evaluates to true.
+    7. By default, the binary modulo (%) operation is given higher precedence than division and multiplication,
+        but changing this is as simple as changing a single value, i.e., rank of Operator#MOD.
+        Example 1 -> By default, "8 / 8 % 6 * 2" is equivalent to "8 / (8 % 6) * 2".
+    ...
+    Most of these quirks can be avoided by simply using parentheses, regardless of precedence.
 */
 
 public final class Evaluator{
 
-    static int floatToInt(float original){
-        //return (int)original;//if truncated
-        return Math.round(original);//if rounded half up
+    public static int floatToInt(double original){
+        return Math.toIntExact(Math.round(original));//if rounded half up
     }
 
     public static int evalInt(String expression){
-        return floatToInt(evaluate(expression).toFloat());
+        return floatToInt(evaluate(expression).toNum());
     }
 
     public static boolean evalBool(String expression){
@@ -43,31 +51,14 @@ public final class Evaluator{
         Stack stack = new Stack(expression);
         Node node = stack.peek();
         do{
-            if(node.isDone()){
-                stack.pop();
-                if(stack.isEmpty()) return node;
-                else node = operate(stack.peek(),node);
+            if(!node.isDone()){
+                node = stack.expand();
+                continue;
             }
-            else if(node.isTerm()) terminate(node);
-            else node = stack.expand();
+            stack.pop();
+            if(stack.isEmpty()) return node;
+            else node = operate(stack.peek(),node);
         }while(true);
-    }
-
-    private static void terminate(Node termNode){
-        String term = termNode.getTerm();
-
-        //numeric constant
-        try{
-            termNode.setValue(Float.parseFloat(term),true);
-            return;
-        }catch(NumberFormatException ignored){}
-
-        //boolean constant
-        if("true".equalsIgnoreCase(term)) termNode.setValue(true);
-        else if("false".equalsIgnoreCase(term)) termNode.setValue(false);
-
-        //identifier
-        else throw new IllegalStateException("Cannot fetch value from identifier \""+term+"\"");//TODO: remove placeholder and fetch value from game-specific identifier
     }
 
     private static Node operate(Node parent, Node child){
@@ -89,33 +80,41 @@ public final class Evaluator{
 
             //TODO: Unary operators may exist in chains, and can be reduced without making a chain of nodes
 
-            case POS -> parent.setValue(child.toFloat(),true);
+            case POS -> parent.setValue(child.toNum(),true);
 
-            case NEG -> parent.setValue(-child.toFloat(),true);
+            case NEG -> parent.setValue(-child.toNum(),true);
 
             case NOT -> parent.setValue(!child.toBool());
 
-            case POW -> parent.setValue((float)Math.pow(parent.toFloat(),child.toFloat()),true);
+            case POW -> parent.setValue(Math.pow(parent.toNum(),child.toNum()),true);
 
-            case LOG -> parent.setValue((float)(Math.log(parent.toFloat())/Math.log(child.toFloat())),true);
+            case LOG -> parent.setValue(logarithm(parent.toNum(),child.toNum()),true);
 
-            case MOD -> parent.setValue(parent.toFloat()%child.toFloat(),true);
+            case MOD -> {
+                double denom = child.toNum();
+                if(denom==0) throw new ArithmeticException("Modulo by zero");
+                else parent.setValue(parent.toNum()%denom,true);
+            }
 
-            case MUL -> parent.setValue(parent.toFloat()*child.toFloat(),true);
+            case MUL -> parent.setValue(parent.toNum()*child.toNum(),true);
 
-            case DIV -> parent.setValue(parent.toFloat()/child.toFloat(),true);
+            case DIV -> {
+                double denom = child.toNum();
+                if(denom==0) throw new ArithmeticException("Division by zero");
+                else parent.setValue(parent.toNum()/denom,true);
+            }
 
-            case ADD -> parent.setValue(parent.toFloat()+child.toFloat(),true);
+            case ADD -> parent.setValue(parent.toNum()+child.toNum(),true);
 
-            case SUB -> parent.setValue(parent.toFloat()-child.toFloat(),true);
+            case SUB -> parent.setValue(parent.toNum()-child.toNum(),true);
 
-            case LOEQ -> parent.setValue(parent.toFloat() <= child.toFloat());
+            case LOEQ -> parent.setValue(parent.toNum() <= child.toNum());
 
-            case MOEQ -> parent.setValue(parent.toFloat() >= child.toFloat());
+            case MOEQ -> parent.setValue(parent.toNum() >= child.toNum());
 
-            case LESS -> parent.setValue(parent.toFloat() < child.toFloat());
+            case LESS -> parent.setValue(parent.toNum() < child.toNum());
 
-            case MORE -> parent.setValue(parent.toFloat() > child.toFloat());
+            case MORE -> parent.setValue(parent.toNum() > child.toNum());
 
             case EQL -> parent.setValue(parent.valueEquals(child));
 
@@ -130,6 +129,54 @@ public final class Evaluator{
         return parent;
     }
 
+    private static double logarithm(double target, double base){
+
+        final double ZERO_BASE = 1.5717277847E-162;
+
+        if(target>0 && base>ZERO_BASE) return Math.log(target)/Math.log(base);
+
+        else if(target==0 || Math.abs(base)<=ZERO_BASE) throw new ArithmeticException("Logarithm with zero");
+
+        else if(base==1 || base==-1) throw new ArithmeticException("Logarithm with base (+/-) one");
+
+        else if(target==1) return 0;
+
+        else if(!Double.isFinite(target) || !Double.isFinite(base)) throw new ArithmeticException("Logarithm with invalid value(s)");
+
+        else if(target==base) return 1;
+
+        else if(base>0) throw new ArithmeticException("Logarithm with positive base and negative target");
+
+        long left=1, right=Long.MAX_VALUE/2;
+        double error=1;
+
+        while(right > left){
+
+            long n = left+(right-left+(error>0?1:0))/2;
+
+            long power = 2*n+(target>0?0:1);//if target pos, try evens, else odds
+
+            double result = Math.pow(base, power);
+            if(target<0 && Double.isInfinite(result)) result = Double.NEGATIVE_INFINITY;//large odd powers of large negative numbers return positive infinity, which is wrong
+
+            error = target - result;
+            if(target<0) error = -error;
+
+            if(Math.abs(error) <= Double.MIN_VALUE) return power;
+
+            if(base < -1){//result increases in magnitude with power
+                if(error > 0) left = n;//target is ahead in magnitude, increase power
+                else right = n;
+            }
+            else{//result increases in magnitude against power
+                if(error > 0) right = n;//target is ahead in magnitude, decrease power
+                else left = n;
+            }
+
+        }
+        throw new ArithmeticException("Logarithm not possible");
+    }
+
     private static Node shortCircuit(Node node){
 
         if(!node.hasValue()) throw new IllegalArgumentException("Cannot short-circuit without value");
@@ -141,8 +188,8 @@ public final class Evaluator{
             //Each case must have exclusivity over the rank of the operators mentioned in it.
             //Operators mentioned together in a case may or may not share rank, but others should not share rank with any of them.
 
-            case POW -> node.toFloat()==0 || node.toFloat()==1;
-            case MOD, MUL, DIV -> node.toFloat()==0;//these operators may or may not share rank, but others should not share rank with any of them
+            case POW -> node.toNum()==0 || node.toNum()==1;
+            case MUL/*, DIV, MOD*/ -> node.toNum()==0;//these operators may or may not share rank, but others should not share rank with any of them
             case AND -> !node.toBool();
             case OR -> node.toBool();
             default -> false;
@@ -184,7 +231,7 @@ final class Stack{
         do{
             node = node.next();
             push(node);
-        }while(!node.isDone() && !node.isTerm());
+        }while(!node.isDone());
         return node;
     }
 
@@ -198,7 +245,7 @@ final class Node{
 
     private Operator operator = null;
     private boolean init = false;
-    private float value = 0;
+    private double value = 0;
     private Type type = Type.AMBI;//because numeric and boolean values are not allowed to interchange
 
     private Node(Queue<String> subexps, Queue<Operator> usedOps, int rank){
@@ -210,10 +257,31 @@ final class Node{
     public Node(String expression){
         expression = disclose(expression);//no null check
         if(expression.isBlank()) throw new IllegalArgumentException("Expression must not be blank");
+
         final int len = expression.length();
         subexps = new ArrayDeque<>(1+len/2);
         usedOps = new ArrayDeque<>(len/2);
         int tempRank = -1;
+
+        /*
+        The following portion that checks whether the expression is a literal, can be moved down to be just above the identifier-fetch,
+        but in that case, while efficiency will increase, floating point literals with signed exponent will not be supported.
+        */
+
+        //if numeric literal
+        try{
+            setValue(Double.parseDouble(expression),true);
+        }catch(NumberFormatException ignored){}
+
+        //if boolean literal
+        if("true".equalsIgnoreCase(expression)) setValue(true);
+        else if("false".equalsIgnoreCase(expression)) setValue(false);
+
+        //if literal
+        if(hasValue()){
+            rank = tempRank;
+            return;
+        }
 
         int subAt=0;
 
@@ -244,12 +312,27 @@ final class Node{
             i=subAt-1;
 
         }
-
         if(subAt==len) throw new IllegalArgumentException("Expression \""+expression+"\" ended with an operator");
         else subexps.add(expression.substring(subAt));
-
-        if(subexps.isEmpty()) throw new IllegalStateException("Something went wrong");
         this.rank = tempRank;
+
+        if(isDone()) throw new IllegalStateException("Node could not be created");
+
+        //TODO: Best place to reduce a chain of unary operators
+
+        if(operator!=null || !usedOps.isEmpty() || subexps.size()!=1){
+            if(rank<0) throw new IllegalStateException("Not a term node, yet has no rank");
+            else return;
+        }
+
+        if(expression.matches("(\\d+\\.?\\d*|\\.\\d+)[eE]|\\d+[fFdD]")){
+            throw new IllegalArgumentException(
+                    "Floating point literal with signed exponent must be enclosed in parentheses: "+expression
+            );
+        }
+
+        //identifier
+        throw new IllegalStateException("Cannot fetch value from identifier \""+expression+"\"");//TODO: remove placeholder and fetch value from game-specific identifier
     }
 
     private static Optional<Operator> operatorAt(int index, String expression, boolean isUnary){
@@ -308,40 +391,52 @@ final class Node{
         return switch(type){
             case AMBI -> this.getValue() == other.getValue();
             case BOOL -> this.toBool() == other.toBool();
-            case NUM -> this.toFloat() == other.toFloat();
+            case NUM -> this.toNum() == other.toNum();
         };
     }
 
-    private float getValue(){
+    private double getValue(){
         if(!hasValue()) throw new NoSuchElementException("No value present");
         return value;
     }
 
-    public float toFloat(){
+    private void setType(Type newType){
+        boolean allowed = switch(type){
+            case AMBI -> true;
+            case BOOL -> newType == Type.BOOL;
+            case NUM -> newType != Type.AMBI;
+        };
+        if(allowed) type = newType;
+        else throw new IllegalArgumentException("Cannot set "+type+" node to "+newType);
+    }
+
+    public double toNum(){
         if(type==Type.BOOL) throw new NoSuchElementException("Boolean value instead of numeric");
-        type = Type.NUM;
+        setType(Type.NUM);
         return getValue();
     }
 
     public boolean toBool(){
         if(type==Type.NUM) throw new NoSuchElementException("Numeric value instead of boolean");
-        type = Type.BOOL;
+        setType(Type.BOOL);
         return Evaluator.floatToInt(getValue())!=0;
     }
 
-    public void setValue(float value, boolean setNumeric){
+    public void setValue(double value, boolean setNumeric){
+        if(Double.isInfinite(value)) throw new IllegalArgumentException("Value too large: "+value);
+        else if(!Double.isFinite(value)) throw new IllegalArgumentException("Unsupported value: "+value);//NaN
         this.value = value;
         init = true;
-        if(setNumeric) type = Type.NUM;
+        if(setNumeric) setType(Type.NUM);
     }
 
     public void setValue(boolean value){
-        type = Type.BOOL;
+        setType(Type.BOOL);
         setValue(value?1:0, false);
     }
 
     public void setValue(Node toCopy){
-        type = toCopy.type;
+        setType(toCopy.type);
         setValue(toCopy.getValue(), false);
     }
 
@@ -365,27 +460,9 @@ final class Node{
         else return true;
     }
 
-    public boolean isTerm(){
-        if(isDone()) return false;
-
-        boolean oneSub = subexps.size()==1;//may yet have one or more unary operators
-        boolean noOps = operator==null && (usedOps==null || usedOps.isEmpty());
-        boolean noRank = rank<0;
-
-        if(oneSub && noOps) return true;
-        else if(noRank) throw new IllegalStateException("Not a term, yet has no rank");
-        else return false;
-    }
-
-    public String getTerm(){
-        if(!isTerm()) throw new NoSuchElementException("Not a term");
-        else return subexps.remove();
-    }
-
     public Node next(){
 
         if(isDone()) throw new NoSuchElementException("Done node has no next");
-        if(isTerm()) throw new NoSuchElementException("Term node has no next");
 
         Queue<String> subSubs = new ArrayDeque<>(subexps.size());
         Queue<Operator> subOps = new ArrayDeque<>(usedOps.size());
